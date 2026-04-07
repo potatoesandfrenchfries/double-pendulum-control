9f8db7396b7f5bd6138169abb8eb7d0420db93ad
Kanish H R <kanishhr407@gmail.com>
2026-04-08 00:19:44 +0100

    Shifting logic to a slightly different system
    Decided to have the pendulum suspended from a cart that can freely move horizontally with no friction
    Recompensated the Lagrangian, and suffered through the derivation again
    
    Force u is applied on the cart, and the torque is dependent on this input.
    
    Yippee

--- physics.jl
+++ physics.jl
@@ -1,36 +1,56 @@
 using DifferentialEquations
-using ControlSystems
-using ForwardDiff
+#using ControlSystems
+#using ForwardDiff
+using LinearAlgebra
 
 struct Parameters
     m1::Float64
     m2::Float64
+    M::Float64
     l1::Float64
     l2::Float64
     g::Float64
+    u::Float64
 end
 
+function doublependulum!(dx, x, p, _)
+    r, dr, theta1, theta2, dtheta1, dtheta2 = x
+    m1, m2, M, l1, l2, g, u = p.m1, p.m2, p.M, p.l1, p.l2, p.g, p.u
 
-function doublependulum!(dx,x,p,t) 
-    theta1, theta2, dtheta1, dtheta2 = x
+    # Mass matrix from Lagrangian (see LaTeX derivation)
+    # Generalised coordinates: [r, theta1, theta2]
+    mass_matrix = [
+        M+m1+m2                  (m1+m2)*l1*cos(theta1)          m2*l2*cos(theta2)            ;
+        (m1+m2)*l1*cos(theta1)   (m1+m2)*l1^2                    m2*l1*l2*cos(theta2-theta1)  ;
+        m2*l2*cos(theta2)         m2*l1*l2*cos(theta2-theta1)    m2*l2^2                      ;    
+    ]
 
-    m1, m2, l1, l2, g = p.m1, p.m2, p.l1, p.l2, p.g
+    # RHS forcing vector
+    f = [
+        (m1+m2)*l1*sin(theta1)*dtheta1^2 + m2*l2*sin(theta2)*dtheta2^2 + u;
+        -m2*l1*l2*sin(theta1-theta2)*dtheta2^2 - (m1+m2)*g*l1*sin(theta1) ;
+         m2*l1*l2*sin(theta1-theta2)*dtheta1^2 - m2*g*l2*sin(theta2)      ;
+    ]
 
-    A = (m1+m2)*(l1^2)
-    B = (m2*l1*l2*cos(theta2-theta1))
-    C = (m2*l1*l2*(dtheta2^2)*sin(theta1-theta2)) + (m1+m2)*g*l1*sin(theta1)
+    # Cramer's Rule
+    D = det(mass_matrix)
+    ddr      = det([f          mass_matrix[:,2]  mass_matrix[:,3]]) / D
+    ddtheta1 = det([mass_matrix[:,1] f           mass_matrix[:,3]]) / D
+    ddtheta2 = det([mass_matrix[:,1] mass_matrix[:,2]  f         ]) / D
 
-    D = (m2*l1*l2*cos(theta2-theta1))
-    E = (m2*l2^2)
-    F = (-m2*l1*l2*(dtheta1^2)*sin(theta1-theta2)) + m2*g*l2*sin(theta2)
-
-    ddtheta1 = (-C*E + B*F)/(A*E - B*D)
-    ddtheta2 = (-A*F + C*D)/(A*E - B*D)
-
-    # TODO: Torque at base for control
-
-    dx[1] = dtheta1
-    dx[2] = dtheta2
-    dx[3] = ddtheta1
-    dx[4] = ddtheta2
+    dx[1] = dr
+    dx[2] = ddr
+    dx[3] = dtheta1
+    dx[4] = dtheta2
+    dx[5] = ddtheta1
+    dx[6] = ddtheta2
 end
+
+# --- Test simulation (no control) ---
+# m1=1, m2=1, M=5, l1=1, l2=1, g=9.81, u=0
+p  = Parameters(1.0, 1.0, 5.0, 1.0, 1.0, 9.81, 0.0)
+x0 = [0.0, 0.0, π+0.1, π-0.1, 0.0, 0.0]   # near upright
+tspan = (0.0, 10.0)
+prob = ODEProblem(doublependulum!, x0, tspan, p)
+sol  = solve(prob, Tsit5())
+println("Solved $(length(sol.t)) time steps, t_end = $(sol.t[end])")
